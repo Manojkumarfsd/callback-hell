fcba69f95f948568e8d7085d22617261cc33f47b
Manojkumarfsd <131541058+Manojkumarfsd@users.noreply.github.com>
2023-12-05 23:05:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+function function1(callback){
+    setTimeout(function(){
+    
+    callback();
+    },1000);
+
+}
+function function2(callback){
+    setTimeout(function(){
+        
+        callback();
+    },1000);
+}
+
+function function3(callback){
+    setTimeout(function(){
+       
+        callback();
+    },1000);
+}
+function function4(callback){
+    setTimeout(function(){
+        
+        callback();
+    },1000);
+}
+function function5(callback){
+    setTimeout(function(){
+        
+        callback();
+    },1000);
+}
+function function6(callback){
+    setTimeout(function(){
+   
+    callback();
+    },1000);
+
+}
+function function7(callback){
+    setTimeout(function(){
+        
+        callback();
+    },1000);
+}
+
+function function8(callback){
+    setTimeout(function(){
+        
+        callback();
+    },1000);
+}
+function function9(callback){
+    setTimeout(function(){
+       
+        callback();
+    },1000);
+}
+function function10(callback){
+    setTimeout(function(){
+       
+        callback();
+    },1000);
+}
+
+function1(function(){
+    console.log("10");
+function2(function(){
+    console.log("9");
+    function3(function(){
+        console.log("8");
+function4(function(){
+    console.log("7");
+    function5(function(){
+        console.log("6");
+        function1(function(){
+            console.log("5");
+        function2(function(){
+            console.log("4");
+            function3(function(){
+                console.log("3");
+        function4(function(){
+            console.log("2");
+            function5(function(){
+                console.log("1");
+        console.log("Happy Indepentence Day")
+
+    });
+    });      
+        
+    });
+    });
+});
+});
+});      
+    
+});
+});
+});
